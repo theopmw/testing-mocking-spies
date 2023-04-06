ca8097ad7d05958b26863e7ad5855cba47444ed7
theopmw <theowrightdev@gmail.com>
2023-04-06 16:37:47 +0100

    Add mock

--- src/util/io.spec.js
+++ src/util/io.spec.js
@@ -1,8 +1,15 @@
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
+import { promises as fs } from 'fs';
 import writeData from './io';
+
+vi.mock('fs');
 
 it('should execute the writeFile method', () => {
   const testData = 'Test';
   const testFileName = 'test.txt';
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+
+  writeData(testData, testFileName);
+
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  expect(fs.writeFile).toBeCalled();
 });
